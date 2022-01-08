04588ff128d84a2fd726e7b1137137cf181271d7
Armağan Gök <1armagangok@gmail.com>
2022-01-08 17:32:23 +0300

    singletone added

--- lib/app/core/theme/colors.dart
+++ lib/app/core/theme/colors.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 final CustomColor customColor = CustomColor();
 
 class CustomColor {
+  static final CustomColor _singleton = CustomColor._internal();
+
+  factory CustomColor() {
+    return _singleton;
+  }
+  CustomColor._internal();
   Color bgColor = const Color.fromRGBO(62, 62, 62, 1);
   Color contColor = const Color(0xFF2B2D32);
 }
