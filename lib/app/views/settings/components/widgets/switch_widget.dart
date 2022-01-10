3a4b47ab5924a1bc16676fc5c5738299e4f56a94
Armağan Gök <1armagangok@gmail.com>
2022-01-10 12:48:24 +0300

    .

---
+++ lib/app/views/settings/components/widgets/switch_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+
+class SwitchWidget extends StatefulWidget {
+  bool val = false;
+  SwitchWidget({
+    Key? key,
+    required this.val,
+  }) : super(key: key);
+
+  @override
+  State<SwitchWidget> createState() => _SwitchWidgetState();
+}
+
+class _SwitchWidgetState extends State<SwitchWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSwitch(
+      value: widget.val,
+      onChanged: (t) {
+        setState(() {
+          widget.val = t;
+        });
+      },
+    );
+  }
+}
--- lib/app/views/settings/setting_view.dart
+++ lib/app/views/settings/setting_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:think_it_up_app/app/core/components/common/alignment/alignment.dart';
+
+import 'package:think_it_up_app/app/views/dashboard/components/export/export.dart';
 import 'package:think_it_up_app/app/views/winners/components/stacks/top_bar.dart';
+
+import 'components/stacks/stacks.dart';
+import 'components/widgets/switch_widget.dart';
 
 class SettingView extends StatelessWidget {
   const SettingView({Key? key}) : super(key: key);
@@ -22,12 +26,17 @@ class SettingView extends StatelessWidget {
               onPressed: () {},
             ),
             SettingItem(
-              text: 'Push notification',
-              onPressed: () {},
-            ),
+                text: 'Push notification',
+                onPressed: () {},
+                icon: SwitchWidget(
+                  val: true,
+                )),
             SettingItem(
               text: 'Dark mode',
               onPressed: () {},
+              icon: SwitchWidget(
+                val: false,
+              ),
             ),
             SettingItem(
               text: 'Get premium',
@@ -57,36 +66,3 @@ class SettingView extends StatelessWidget {
   }
 }
 
-class SettingItem extends StatelessWidget {
-  final String text;
-  final Widget? icon;
-  final Function onPressed;
-
-  const SettingItem({
-    Key? key,
-    required this.text,
-    this.icon,
-    required this.onPressed,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => onPressed(),
-      child: Padding5(
-        child: CustomContainer(
-          heigth: 40,
-          child: SymmetricPaddingH5(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(text),
-                icon ?? const Icon(Icons.arrow_forward_ios_rounded, size: 30),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
